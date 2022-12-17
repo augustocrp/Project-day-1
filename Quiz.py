f0b9caa0957e27d073308d1d9a1f45ed80b75a77
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-17 14:29:24 -0300

    Update Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,14 @@
+print("Seja muito bem vindo ao quiz da Biblia!")
+answer_user = input("Quer começar? (S/N) ")
+
+if answer_user != "S":
+    quit()
+
+print("Começando...")
+print("Quem foi o primeiro Nazireu na história da biblia? \n (A) Abraão \n (B) Davi \n (C) Sansão \n (D) Samuel")
+answer_1 = input("Resposta: ")
+
+if answer_1 =="C"
+    print("Correto!")
+else: 
+    print("Incorreto!")
